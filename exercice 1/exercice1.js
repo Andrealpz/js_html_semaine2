873d7311429b90b3d45e4966dc8dbc7c94398c35
MARTINEZ Kévin <msi03@laposte.net>
2016-12-11 21:55:29 +0100

    exercice1_6

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,7 +1,5 @@
-var age = prompt('Quel est ton age?');
-if(age>=18) {
-	alert('Vous etes majeur');
-}
-else{
-	alert('vous etes mineure');
-}
+var Nom = prompt("Quel est ton nom?");
+var Prénom = prompt("Quel est ton prénom?");
+var Ville = prompt("Dans quelle ville habite tu ?");
+
+alert(' Nom: ' + Nom + '\n Prénom: ' + Prénom + '\n Ville: ' + Ville);
